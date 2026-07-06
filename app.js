801af3ddba5c0eb3017e533af1159740d7e07299
MemeClass Teacher <teacher@memeclass.com>
2026-07-06 08:09:11 -0300

    Reduce size of bouncing memes by 30%

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ class DVDBouncer {
             if (item.isPfp) {
                 img.className = 'w-16 h-16 md:w-24 md:h-24 rounded-full border-4 border-white shadow-lg object-cover';
             } else {
-                img.className = 'w-32 h-32 md:w-48 md:h-48 rounded-2xl border-4 border-white shadow-lg object-cover';
+                img.className = 'w-24 h-24 md:w-32 md:h-32 rounded-2xl border-4 border-white shadow-lg object-cover';
             }
             el.appendChild(img);
             
@@ -105,8 +105,8 @@ class DVDBouncer {
     animate() {
         this.items.forEach(item => {
             if (item.w === 0) {
-                item.w = item.el.offsetWidth || 192; // md:w-48 is 192px
-                item.h = item.el.offsetHeight || 192;
+                item.w = item.el.offsetWidth || 128; // md:w-32 is 128px
+                item.h = item.el.offsetHeight || 128;
             }
 
             item.x += item.vx;
